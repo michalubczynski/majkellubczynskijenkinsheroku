17156392eef14543ff082caaf3520f0962700ca0
michalubczynski <michlub322@student.polsl.pl>
2023-04-25 13:47:40 +0200

    Initial commit

---
+++ src/main/java/com/example/majkellubczynskijenkinsheroku/MajkelLubczynskiJenkinsHerokuApplication.java
@@ -0,0 +1,17 @@
+package com.example.majkellubczynskijenkinsheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class MajkelLubczynskiJenkinsHerokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajkelLubczynskiJenkinsHerokuApplication.class, args);
+    }
+    @GetMapping
+    public String get(){
+    return "Hello Jenkins";
+    }
+}
